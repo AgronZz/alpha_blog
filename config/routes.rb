f48d7cabb325ae4d5b7a6d7db142796914dec260
AgronZz <agronzzberisha@hotmail.com>
2025-05-07 22:14:16 +0200

    Adding routes index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "home/index"
 
-  resources :articles, only: [:show]
+  resources :articles , only: [:show, :index]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
